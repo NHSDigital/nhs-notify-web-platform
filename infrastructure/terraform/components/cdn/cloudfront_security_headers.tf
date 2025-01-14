11024556ea0dc782149ae0e00307cd1a9c39079c
ClareJonesBJSS <clare.jones75@nhs.net>
2025-01-14 10:03:05 +0000

    CCM-5340: Security header changes

---
+++ infrastructure/terraform/components/cdn/cloudfront_security_headers.tf
@@ -0,0 +1,28 @@
+resource "aws_cloudfront_response_headers_policy" "security_headers" {
+  name = "security-headers-policy"
+
+  remove_headers_config {
+    items {
+      header = "x-powered-by"
+    }
+    items {
+      header = "access-control-allow-origin"
+    }
+  }
+
+  security_headers_config {
+    content_security_policy {
+      content_security_policy = "base-uri 'self'; form-action 'self'; frame-ancestors 'none'; default-src 'none'; connect-src 'self'; font-src 'self' https://assets.nhs.uk; img-src 'self'; script-src 'self'; style-src 'self'; upgrade-insecure-requests"
+      override = false
+    }
+    content_type_options {
+      override = true
+    }
+    strict_transport_security {
+      access_control_max_age_sec = "31536000"
+      include_subdomains = true
+      preload = true
+      override = true
+    }
+  }
+}
