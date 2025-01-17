1c5fccbb49dd7fa500ec0b6ca163aa9f4706f3d4
Alex Nuttall <alex.nuttall1@nhs.net>
2025-01-17 13:50:57 +0000

    CCM-5340: add Cross-Origin-Resource-Policy

--- infrastructure/terraform/components/cdn/cloudfront_security_headers.tf
+++ infrastructure/terraform/components/cdn/cloudfront_security_headers.tf
@@ -13,16 +13,25 @@ resource "aws_cloudfront_response_headers_policy" "security_headers" {
   security_headers_config {
     content_security_policy {
       content_security_policy = "base-uri 'self'; form-action 'self'; frame-ancestors 'none'; default-src 'none'; connect-src 'self'; font-src 'self' https://assets.nhs.uk; img-src 'self'; script-src 'self'; style-src 'self'; upgrade-insecure-requests"
-      override = false
+      override                = false
     }
     content_type_options {
+      # micro-frontend middleware must insert an overriding policy due to nextjs inline scripts
       override = true
     }
     strict_transport_security {
       access_control_max_age_sec = "31536000"
-      include_subdomains = true
-      preload = true
-      override = true
+      include_subdomains         = true
+      preload                    = true
+      override                   = true
+    }
+  }
+
+  custom_headers_config {
+    items {
+      header   = "Cross-Origin-Resource-Policy"
+      override = false
+      value    = "same-origin"
     }
   }
 }
