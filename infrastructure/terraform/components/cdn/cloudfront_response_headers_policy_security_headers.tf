f67c41f70f82390925d185089fb19d1abaf4d7ef
Alex Nuttall <alex.nuttall1@nhs.net>
2025-02-07 16:08:31 +0000

    CCM-5340: add comments linking to zap alerts

--- infrastructure/terraform/components/cdn/cloudfront_response_headers_policy_security_headers.tf
+++ infrastructure/terraform/components/cdn/cloudfront_response_headers_policy_security_headers.tf
@@ -3,14 +3,17 @@ resource "aws_cloudfront_response_headers_policy" "security_headers" {
 
   remove_headers_config {
     items {
+      #  https://www.zaproxy.org/docs/alerts/10037/
       header = "x-powered-by"
     }
     items {
+      # static pages have a wildcard value by default, removing this enforces 'same-origin'
       header = "access-control-allow-origin"
     }
   }
 
   security_headers_config {
+    # https://www.zaproxy.org/docs/alerts/10038-1/
     content_security_policy {
       content_security_policy = "base-uri 'self'; form-action 'self'; frame-ancestors 'none'; default-src 'none'; connect-src 'self'; font-src 'self' https://assets.nhs.uk; img-src 'self'; script-src 'self'; style-src 'self'; upgrade-insecure-requests"
       override                = false
@@ -19,6 +22,7 @@ resource "aws_cloudfront_response_headers_policy" "security_headers" {
       # micro-frontend middleware must insert an overriding policy due to nextjs inline scripts
       override = true
     }
+    # https://www.zaproxy.org/docs/alerts/10035-1/
     strict_transport_security {
       access_control_max_age_sec = "31536000"
       include_subdomains         = true
@@ -28,6 +32,7 @@ resource "aws_cloudfront_response_headers_policy" "security_headers" {
   }
 
   custom_headers_config {
+    # https://www.zaproxy.org/docs/alerts/90004/
     items {
       header   = "Cross-Origin-Resource-Policy"
       override = false
@@ -47,6 +52,7 @@ resource "aws_cloudfront_response_headers_policy" "security_headers" {
     }
 
     items {
+      # https://www.zaproxy.org/docs/alerts/10063-1/
       header   = "Permissions-Policy"
       override = false
       value    = "geolocation=(), microphone=(), camera=(), payment=(), fullscreen=()"
